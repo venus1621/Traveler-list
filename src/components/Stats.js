daa8bb45a87def9007ebc82f0442a28b5c4198e8
venus1621 <166075738+venus1621@users.noreply.github.com>
2024-05-06 02:45:33 -0700

    Update Stats.js

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -17,7 +17,8 @@ export default function Stats({ items }) {
           ? "You got everything! Ready to go ✈️"
           : ` 💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
       </em>
-    <p> Venus Hailemeskel </p>
+    <br/>
+    <p> Venus Hailemeskel.....</p>
     </footer>
   );
 }
